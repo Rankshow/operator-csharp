4e5dca9f2b8ba615ca097d1723b5d3d089e8c524
rankshow <uwaomaobinna20@gmail.com>
2023-09-03 17:54:13 +0100

    Arithmetic operation

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Operators;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int result =  newValue()
+       ;
+    //    if ( result <= 20)
+    //    {
+    //    Console.WriteLine("Yes, it is...");
+    //    }
+    //    else Console.WriteLine("Hmm! wrong answer...");    
+     Console.WriteLine($"Nice {result}");     
+    }
+    private static int getIncrement()
+    {
+        int a = 21;
+        --a;
+        return a;
+    }
+    private static int newValue()
+    {
+        // int b = 40;
+        // int c = 11;
+        // b *= c;
+        // return b;
+        int x = 20;
+        x--;
+        return x;
+    }
+}
